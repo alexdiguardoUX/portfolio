4414fd43aab2729bf60c1a6d29c21324acab538d
Alex Di Guardo <alex@alexs-imac.mynet>
2020-12-08 16:22:16 +0000

    added daytrip challenge

--- scripts.js
+++ scripts.js
@@ -4,18 +4,21 @@ var modalSecondLife = document.getElementById('modal-secondlife');
 var modalSubNav = document.getElementById('modal-sub-nav');
 var modalBrandReplace = document.getElementById('modal-brand-replacement');
 var modalCostco = document.getElementById('modal-costco');
+var modalDaytrip = document.getElementById('modal-daytrip');
 
 // Get the X that closes the modal
 var closeSecondLife = document.getElementsByClassName("close_secondlife_modal")[0];
 var closeSubNav = document.getElementsByClassName("close_sub-nav_modal")[0];
 var closeBrandReplace = document.getElementsByClassName("close_brand-replacement_modal")[0];
 var closeCostco = document.getElementsByClassName("close_costco_modal")[0];
+var closeDaytrip = document.getElementsByClassName("close_daytrip_modal")[0];
 
 // Clickable thumbnails
 var secondLifeThumb = document.getElementsByClassName("secondlife-thumb")[0];
 var subNavThumb = document.getElementsByClassName("sub-nav-thumb")[0];
 var brandReplaceThumb = document.getElementsByClassName("brand-replacement-thumb")[0];
 var costcoThumb = document.getElementsByClassName("costco-thumb")[0];
+var daytripThumb = document.getElementsByClassName("daytrip-thumb")[0];
 
 // Specify the display style
 secondLifeThumb.onclick = function openModal() {
@@ -34,6 +37,10 @@ costcoThumb.onclick = function openModal() {
     modalCostco.style.display = "block";
 }
 
+daytripThumb.onclick = function openModal() {
+    modalDaytrip.style.display = "block";
+}
+
 // When the user clicks on X, close the modal
 closeSecondLife.onclick = function() {
     modalSecondLife.style.display = "none";
@@ -49,4 +56,8 @@ closeBrandReplace.onclick = function() {
 
 closeCostco.onclick = function() {
     modalCostco.style.display = "none";
+}
+
+closeDaytrip.onclick = function() {
+    modalDaytrip.style.display = "none";
 }
