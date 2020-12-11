fd8db5fb8326d71d0dd96db1fc69782c8ea5363f
Alex Di Guardo <alex@alexs-imac.mynet>
2020-12-11 23:57:59 +0000

    copy email script

--- scripts.js
+++ scripts.js
@@ -61,3 +61,20 @@ closeCostco.onclick = function() {
 closeDaytrip.onclick = function() {
     modalDaytrip.style.display = "none";
 }
+
+function copyEmail() {
+    var copyText = document.getElementById("emailInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    var tooltip = document.getElementById("myTooltip-email");
+    tooltip.innerHTML = "Email copied to clipboard!";
+}
+  
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip-email");
+    tooltip.innerHTML = "Copy email to clipboard";
+}
+
+
